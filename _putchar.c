ad2d3bc7da7f9b447b286f55dc54024ed927873a
Bekalu Endrias <bekalu2054@gmail.com>
2022-10-16 22:18:01 +0300

    fix

--- _putchar.c
+++ _putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _stdout - Prints out a char to the standard output
+ * _putchar - Prints out a char to the standard output
  * @c: Char to be printed
  *
  * Return: no error
