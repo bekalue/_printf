3a89370f29f0605b7446f45a77074ec5b58bae3d
Bekalu Endrias <bekalu2054@gmail.com>
2022-10-16 22:16:18 +0300

    yep

--- main.h
+++ main.h
@@ -24,22 +24,8 @@ typedef struct print print_f;
 /* Functions */
 int _putchar(char);
 int _printf(const char *format, ...);
-int print_aux(const char *format, print_f print[], va_list args);
 int print_string(va_list);
-int print_String(va_list);
-int print_pointer(va_list);
-int print_int(va_list);
 int print_percentage(va_list);
-int print_unsigned_int(va_list);
-int print_binary(va_list);
 int print_char(va_list);
-int print_octal(va_list);
-int print_hexadecimal(va_list);
-int print_heXadecimal(va_list);
-int print_reversed(va_list);
-int print_rot13(va_list);
-int power(int, int);
-int print_hexadecimal_aux(unsigned long int);
-int print_heXadecimaln(unsigned int n);
 
 #endif
