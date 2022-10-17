128cc4adda413c4e50fd5e8125da6406892ff164
Bekalu Endrias <bekalu2054@gmail.com>
2022-10-17 15:14:55 +0300

    j

--- task3_function.c
+++ task3_function.c
@@ -62,6 +62,45 @@ int print_unsigned_int(va_list u)
     return (count);
 }
 /**
+ * print_hex - prints an unsigned int in hexidecimal form
+ * @n: unsigned int to print
+ * @c: flag to determine case of printing (0 = lower, 1 = upper)
+ *
+ * Return: number of digits printed
+ */
+int print_hex(unsigned int n, unsigned int c)
+{
+    unsigned int a[8];
+    unsigned int i, m, sum;
+    char diff;
+    int count;
+
+    m = 268435456; /* (16 ^ 7) */
+    if (c)
+        diff = 'A' - ':';
+    else
+        diff = 'a' - ':';
+    a[0] = n / m;
+    for (i = 1; i < 8; i++)
+    {
+        m /= 16;
+        a[i] = (n / m) % 16;
+    }
+    for (i = 0, sum = 0, count = 0; i < 8; i++)
+    {
+        sum += a[i];
+        if (sum || i == 7)
+        {
+            if (a[i] < 10)
+                _putchar('0' + a[i]);
+            else
+                _putchar('0' + diff + a[i]);
+            count++;
+        }
+    }
+    return (count);
+}
+/**
  * print_hexadecimal - takes an unsigned int and prints it in lowercase hex notation
  * @x: unsigned int to print
  *
