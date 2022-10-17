cedab5b7c46fc21bdea7bb6b375bfeeacdd99ab3
Bekalu Endrias <bekalu2054@gmail.com>
2022-10-17 14:50:41 +0300

    revert

--- _printf.c
+++ _printf.c
@@ -7,24 +7,15 @@
  */
 int (*find_function(const char *format))(va_list)
 {
-
-	print_f find_f[] = {
+	print_f find_f[] ={
 		{"c", print_char},
-		{"%", print_percentage},
 		{"s", print_string},
-		{"i", print_integer},
-		{"d", print_decimal},
-		{"b", print_binary},
-		{"u", print_unsigned_int},
-		{"o", print_octal},
-		{"x", print_hexadecimal},
-		{"X", print_heXadecimal},
-		{"r", print_reversed},
-		{"S", print_String},
-		{"p", print_address},
-		{"R", print_rot13},
-		{NULL, NULL}};
-	int i = 0;
+		{"%", print_percentage},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
 	while (find_f[i].s)
 	{
 		if (find_f[i].s[0] == (*format))
@@ -44,7 +35,7 @@ int _printf(const char *format, ...)
 {
 	va_list ap;
 	int (*f)(va_list);
-	unsigned int i = 0, count = 0;
+	unsigned int i = 0, cprint = 0;
 
 	if (format == NULL)
 		return (-1);
@@ -54,27 +45,27 @@ int _printf(const char *format, ...)
 		while (format[i] != '%' && format[i])
 		{
 			_putchar(format[i]);
-			count++;
+			cprint++;
 			i++;
 		}
 		if (format[i] == '\0')
-			return (count);
+			return (cprint);
 		f = find_function(&format[i + 1]);
 		if (f != NULL)
 		{
-			count += f(ap);
+			cprint += f(ap);
 			i += 2;
 			continue;
 		}
 		if (!format[i + 1])
 			return (-1);
 		_putchar(format[i]);
-		count++;
+		cprint++;
 		if (format[i + 1] == '%')
 			i += 2;
 		else
 			i++;
 	}
 	va_end(ap);
-	return (count);
+	return (cprint);
 }
